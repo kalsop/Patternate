9c45c0b829ef51bf2bba735b7a2272fdb886a27e
Katherine Alsop <katherine.alsop@digital.cabinet-office.gov.uk>
2013-08-24 21:43:39 +0200

    Al/Katherine: Edit page now checks selected sizes

--- update.php
+++ update.php
@@ -13,66 +13,146 @@
 	  
 	  <?php
 	  
-	  //Get selected pattern
+	  // Get selected pattern
 	  
-
-  
   	$query = "SELECT * FROM patterns where id=7";
     $result= mysql_query($query) or die(mysql_error());
     
     
     while ($row = mysql_fetch_assoc($result)) { 
      $rows[] = $row; 
-   } 
+    } 
    
-   $patternID = $rows[0]['id'];
-   $patternNumber = $rows[0]['pattern_number'];
-   $patternCompanyID = $rows[0]['pattern_company_id'];
+    // Set selected pattern varaibles for later use
+    $patternID = $rows[0]['id'];
+    $patternNumber = $rows[0]['pattern_number'];
+    $patternCompanyID = $rows[0]['pattern_company_id'];
+    $sizesExplode = $rows[0]['sizes_id'];  
+    $sizes_id_array = explode( ',', $sizesExplode );
 
 	  
 	  
 	  
 	  ?>
 	  
-	  <label>Pattern company</label>
-	  <select>
-	  <?php
+	  <form action="update.php" method="post">
+	    <label>Pattern company</label>
+	    <select>
+	    <?php
   
-  	$getPatternCompaniesQuery = "SELECT * FROM pattern_company";
-    $getPatternCompaniesResult= mysql_query($getPatternCompaniesQuery) or die(mysql_error());
+      // Get list of pattern companies
     
-     while ($getPatternCompaniesRow = mysql_fetch_assoc($getPatternCompaniesResult)) { 
-     $getPatternCompaniesRows[] = $getPatternCompaniesRow; 
-   } 
-   
-   for($i=0;$i<count($getPatternCompaniesRows);$i++) { 
-     
-     
-     
-     
-     $id = $getPatternCompaniesRows[$i]['id'];
-     $name = $getPatternCompaniesRows[$i]['name'];
-     
-     echo '<option value="';
-     
-     echo $id . '" ';
-     
-     if ($patternCompanyID == $id ) {
-       echo 'selected ';
+    	$getPatternCompaniesQuery = "SELECT * FROM pattern_company";
+      $getPatternCompaniesResult= mysql_query($getPatternCompaniesQuery) or die(mysql_error());
+    
+       while ($getPatternCompaniesRow = mysql_fetch_assoc($getPatternCompaniesResult)) { 
+       $getPatternCompaniesRows[] = $getPatternCompaniesRow; 
      } 
+   
+     for($i=0;$i<count($getPatternCompaniesRows);$i++) { 
      
-     echo '>';
-     echo $name;
-     echo '</option>';
      
-     }
+     
+     
+       $id = $getPatternCompaniesRows[$i]['id'];
+       $name = $getPatternCompaniesRows[$i]['name'];
+     
+       echo '<option value="';
+     
+       echo $id . '" ';
+     
+       // Pre-select existing pattern company
+     
+       if ($patternCompanyID == $id ) {
+         echo 'selected ';
+       } 
+     
+       echo '>';
+       echo $name;
+       echo '</option>';
+     
+       }
 
 
   
 
-?>
+       ?>
 
-    </select>
+      </select>
+    
+      <label for="patternNumber">Pattern number</label>
+      <input type="text" id="patternNumber" value="<?php echo $patternNumber; ?>">
+      
+      <legend>Sizes</legend>
+        
+        <?php
+  
+      // Get list of sizes
+    
+        $getSizesQuery = "SELECT * FROM sizes order by name ASC";
+                      $getSizesResult= mysql_query($getSizesQuery) or die(mysql_error());
+                    
+                       while ($getSizesRow = mysql_fetch_assoc($getSizesResult)) { 
+                       $getSizesRows[] = $getSizesRow; 
+                     } 
+                   
+                     for($j=0;$j<count($getSizesRows);$j++) { 
+                     
+                     
+                     
+                     
+                       $sizesID = $getSizesRows[$j]['id'];
+                       $sizesName = $getSizesRows[$j]['name'];
+                       //echo $sizesID . " ";
+                       //echo $sizesName . "   ";
+                   
+             
+                       echo '<label for="' . $sizesID . '">';
+                     
+                       echo $sizesName;
+                       
+                       echo '</label>';
+                       
+                       echo '<input type="checkbox" value="' . $sizesID . '" id="' . $sizesID . '"';
+
+                       // Pre-select existing sizes
+                       // 1. Get array of sizes from the selected pattern - ($sizesExplode)
+                       // 2. Cycle through the array and check when printing each size from the available size list
+                        
+                        for ($n=0;$n<count($sizes_id_array);++$n) {
+                            if ($sizes_id_array[$n] == $sizesID) {
+                                echo 'checked ';
+                            } 
+                        }
+                        
+                        
+                        // 3. If the size ID is in the array, then check the check box 
+                       
+                       echo '>';
+                       
+ 
+                       
+                     
+
+                        
+                        
+                        
+                      // if ($sizesID == $id ) {
+                    //     echo 'checked ';
+                     //  } 
+                     
+                   //    echo '>';
+                       
+                     
+                       }
+                
+                
+                  
+                
+                       ?>
+    
+      <input type="submit" >
+    </form>
  
 
 	  
