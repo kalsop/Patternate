d0012716100db1ba07498d55225c992a2908362c
Katherine Alsop <katherine.alsop@digital.cabinet-office.gov.uk>
2013-08-25 00:24:59 +0200

    Edit page now checks selected fabrics, garment type and pattern for

--- update.php
+++ update.php
@@ -15,7 +15,7 @@
 	  
 	  // Get selected pattern
 	  
-  	$query = "SELECT * FROM patterns where id=7";
+  	$query = "SELECT * FROM patterns where id=5";
     $result= mysql_query($query) or die(mysql_error());
     
     
@@ -29,6 +29,12 @@
     $patternCompanyID = $rows[0]['pattern_company_id'];
     $sizesExplode = $rows[0]['sizes_id'];  
     $sizes_id_array = explode( ',', $sizesExplode );
+    $garmentTypeExplode = $rows[0]['garment_type_id'];
+    $garmentType_id_array = explode( ',', $garmentTypeExplode );
+    $patternForExplode = $rows[0]['pattern_for_id'];
+    $pattern_for_id_array = explode( ',', $patternForExplode );
+    $fabricExplode = $rows[0]['fabric_id'];
+    $fabric_id_array = explode( ',', $fabricExplode );
 
 	  
 	  
@@ -36,8 +42,8 @@
 	  ?>
 	  
 	  <form action="update.php" method="post">
-	    <label>Pattern company</label>
-	    <select>
+	    <label for="patternCompany">Pattern company</label>
+	    <select id="patternCompany">
 	    <?php
   
       // Get list of pattern companies
@@ -56,6 +62,7 @@
      
        $id = $getPatternCompaniesRows[$i]['id'];
        $name = $getPatternCompaniesRows[$i]['name'];
+       $slug = $getPatternCompaniesRows[$i]['slug'];
      
        echo '<option value="';
      
@@ -98,9 +105,6 @@
                    
                      for($j=0;$j<count($getSizesRows);$j++) { 
                      
-                     
-                     
-                     
                        $sizesID = $getSizesRows[$j]['id'];
                        $sizesName = $getSizesRows[$j]['name'];
                        //echo $sizesID . " ";
@@ -118,6 +122,7 @@
                        // Pre-select existing sizes
                        // 1. Get array of sizes from the selected pattern - ($sizesExplode)
                        // 2. Cycle through the array and check when printing each size from the available size list
+                       // 3. If the size ID is in the array, then check the check box 
                         
                         for ($n=0;$n<count($sizes_id_array);++$n) {
                             if ($sizes_id_array[$n] == $sizesID) {
@@ -125,31 +130,151 @@
                             } 
                         }
                         
-                        
-                        // 3. If the size ID is in the array, then check the check box 
-                       
-                       echo '>';
-                       
- 
-                       
-                     
+                        echo '>';
 
-                        
-                        
-                        
-                      // if ($sizesID == $id ) {
-                    //     echo 'checked ';
-                     //  } 
+                    }
+                    ?>
+                
+                <legend>Garment type</legend>
+        
+        <?php
+  
+  
+        // Get garment types
+    
+        $getGarmentTypeQuery = "SELECT * FROM garment_type order by name ASC";
+                      $getGarmentTypeResult= mysql_query($getGarmentTypeQuery) or die(mysql_error());
+                    
+                       while ($getGarmentTypeRow = mysql_fetch_assoc($getGarmentTypeResult)) { 
+                       $getGarmentTypeRows[] = $getGarmentTypeRow; 
+                     } 
+                   
+                     for($m=0;$m<count($getGarmentTypeRows);$m++) { 
                      
-                   //    echo '>';
+                       $garmentTypeID = $getGarmentTypeRows[$m]['id'];
+                       $garmentTypeName = $getGarmentTypeRows[$m]['name'];
+                       $garmentTypeSlug = $getGarmentTypeRows[$m]['slug'];
+             
+                       echo '<label for="' . $garmentTypeSlug . '">';
+                     
+                       echo $garmentTypeName;
                        
-                     
-                       }
+                       echo '</label>';
+                       
+                       echo '<input type="checkbox" value="' . $garmentTypeID . '" id="' . $garmentTypeSlug . '"';
+
+                       // Pre-select existing sizes
+                       // 1. Get array of sizes from the selected pattern - ($sizesExplode)
+                       // 2. Cycle through the array and check when printing each size from the available size list
+                       // 3. If the size ID is in the array, then check the check box 
+                        
+                        for ($n=0;$n<count($garmentType_id_array);++$n) {
+                            if ($garmentType_id_array[$n] == $garmentTypeID) {
+                                echo 'checked ';
+                            } 
+                        }
+                        
+                        echo '>';
+
+                    }
                 
                 
                   
                 
-                       ?>
+                      
+  
+  
+      ?>
+      
+      <legend>Pattern for</legend>
+      
+      <?php 
+      
+      // Get pattern for
+    
+        $getPatternForQuery = "SELECT * FROM pattern_for order by name ASC";
+                      $getPatternForResult= mysql_query($getPatternForQuery) or die(mysql_error());
+                    
+                       while ($getPatternForRow = mysql_fetch_assoc($getPatternForResult)) { 
+                       $getPatternForRows[] = $getPatternForRow; 
+                     } 
+                   
+                     for($o=0;$o<count($getPatternForRows);$o++) { 
+                     
+                       $patternForID = $getPatternForRows[$o]['id'];
+                       $patternForName = $getPatternForRows[$o]['name'];
+                       $patternForSlug = $getPatternForRows[$o]['slug'];
+             
+                       echo '<label for="' . $patternForSlug . '">';
+                     
+                       echo $patternForName;
+                       
+                       echo '</label>';
+                       
+                       echo '<input type="checkbox" value="' . $patternForID . '" id="' . $patternForSlug . '"';
+
+                       // Pre-select existing sizes
+                       // 1. Get array of sizes from the selected pattern - ($sizesExplode)
+                       // 2. Cycle through the array and check when printing each size from the available size list
+                       // 3. If the size ID is in the array, then check the check box 
+                        
+                        for ($p=0;$p<count($pattern_for_id_array);++$p) {
+                            if ($pattern_for_id_array[$p] == $patternForID) {
+                                echo 'checked ';
+                            } else {
+                                echo 'djsklj ';
+                            } 
+                        }
+                        
+                        echo '>';
+
+                    }
+                    ?>
+                    
+            <legend>Fabric</legend>
+      
+      <?php 
+      
+      // Get fabric
+    
+        $getFabricQuery = "SELECT * FROM fabrics order by name ASC";
+                      $getFabricResult= mysql_query($getFabricQuery) or die(mysql_error());
+                    
+                       while ($getFabricRow = mysql_fetch_assoc($getFabricResult)) { 
+                       $getFabricRows[] = $getFabricRow; 
+                     } 
+                   
+                     for($q=0;$q<count($getFabricRows);$q++) { 
+                     
+                       $fabricID = $getFabricRows[$q]['id'];
+                       $fabricName = $getFabricRows[$q]['name'];
+                       $fabricSlug = $getFabricRows[$q]['slug'];
+             
+                       echo '<label for="' . $fabricSlug . '">';
+                     
+                       echo $fabricName;
+                       
+                       echo '</label>';
+                       
+                       echo '<input type="checkbox" value="' . $fabricID . '" id="' . $fabricSlug . '"';
+
+                       // Pre-select existing sizes
+                       // 1. Get array of sizes from the selected pattern - ($sizesExplode)
+                       // 2. Cycle through the array and check when printing each size from the available size list
+                       // 3. If the size ID is in the array, then check the check box 
+                        
+                        for ($r=0;$r<count($fabric_id_array);++$r) {
+                            if ($fabric_id_array[$r] == $fabricID) {
+                                echo 'checked ';
+                            } else {
+                                echo 'djsklj ';
+                            } 
+                        }
+                        
+                        echo '>';
+
+                    }
+                    ?>
     
       <input type="submit" >
     </form>
